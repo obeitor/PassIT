d1c6873df9d1d9be1c75a691e607459b32aa0913
Abdulgafar Obeitor <aobeitor@yahoo.com>
2016-11-16 12:48:17 +0100

    Update ActivateProduct.java
    Added the background caller activity in the onclick function, also have overriden the onPause called just before this activity is closed to store the activation state of the application

--- app/src/main/java/com/ObtSoft/passit/app/ActivateProduct.java
+++ app/src/main/java/com/ObtSoft/passit/app/ActivateProduct.java
@@ -24,28 +24,23 @@ public class ActivateProduct extends ActionBarActivity {
         btnActivate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Thats all
+                String entry = edActivate.getText().toString();
+                if(entry.isEmpty()){
+                    edActivate.setError("Please enter the code");
+                }
+                else{
+                    ActivateBackground.createBackgroundActivity(activity,entry);
+                }
             }
         });
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activate_product, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences preferences = getSharedPreferences(MainActivity.PREF,MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(MainActivity.ACTIVATED,MainActivity.getActivationStatus());
+        editor.commit();
     }
 }
